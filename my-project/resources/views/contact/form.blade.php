581a2a3ee52a606b2bade3988848ff48a976908c
Arturo Lume <edwin.lume@tecsup.edu.pe>
2023-04-01 22:24:18 -0500

    CRUD en Laravel con Docker

---
+++ my-project/resources/views/contact/form.blade.php
@@ -0,0 +1,19 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('Nombre') }}
+            {{ Form::text('Nombre', $contact->Nombre, ['class' => 'form-control' . ($errors->has('Nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('Nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('Celular') }}
+            {{ Form::text('Celular', $contact->Celular, ['class' => 'form-control' . ($errors->has('Celular') ? ' is-invalid' : ''), 'placeholder' => 'Celular']) }}
+            {!! $errors->first('Celular', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
